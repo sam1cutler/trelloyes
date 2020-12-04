ceba8359729173c6ba57c2a483f1264d1cec8f0a
Sam Cutler <sam.cutler@gmail.com>
2020-12-04 12:41:58 -0800

    Refactored to use state rather than prop for the store

--- src/App.js
+++ src/App.js
@@ -2,43 +2,83 @@ import React from 'react';
 import './App.css';
 import List from './List.js';
 
-function App(props) {
+class App extends React.Component {
   
-  //console.log('App.js ran, with the following props:')
-  //console.log(props);
-
-  // Generate an array of List Component calls (?) with the appropriate
-  //    'header' and 'cards' props...
-  const arrayOfLists = props.store.lists.map( (item) => {
-    const headerValue = item.header;
-    //console.log(`Current list is ${headerValue}.`);
-    //console.log()
-    
-    const cardsValue = item.cardIds.map( 
-      (activeCard) => props.store.allCards[activeCard]);
-    //console.log('Array of card information for this list is:')
-    //console.log(cardsValue);
+  // Establish the state containing the store
+  state = {
+    lists: [
+      {
+        id: '1',
+        header: 'First list',
+        cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+      },
+      {
+        id: '2',
+        header: 'Second list',
+        cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+      },
+      {
+        id: '3',
+        header: 'Third list',
+        cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+      },
+      {
+        id: '4',
+        header: 'Fourth list',
+        cardIds: [ 'l', 'm' ],
+      },
+    ],
+    allCards: {
+      'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+      'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+      'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+      'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+      'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+      'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+      'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+      'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+      'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+      'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+      'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+      'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+      'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+    },
+  }
   
+  // Render array of List components w/ 'header' and 'cards' props
+  makeListArray() {
     return (
-      <li key={item.id} className='List'>
-        <List header={headerValue} cards={cardsValue} />
-      </li>
+      this.state.lists.map( (item) => {
+        const headerValue = item.header;
+        
+        const cardsValue = item.cardIds.map( 
+          (activeCard) => this.state.allCards[activeCard]);
+        
+        return (
+          <li key={item.id} className='List'>
+            <List 
+              header={headerValue} 
+              cards={cardsValue} 
+            />
+          </li>
+        );
+      })
+    ) 
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloyes!</h1>
+        </header>
+         <ul className='App-list'>
+            {this.makeListArray()}
+        </ul>
+      </main>
     );
-  });
+  }
 
-
-  //console.log(arrayOfLists);
-
-  return (
-    <main className='App'>
-      <header className='App-header'>
-        <h1>Trelloyes!</h1>
-      </header>
-       <ul className='App-list'>
-          {arrayOfLists}
-      </ul>
-    </main>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import STORE from './store.js';
-
-console.log('index.js ran, at least in part');
 
 ReactDOM.render(
-  <App 
-    store={STORE}
-  />,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
